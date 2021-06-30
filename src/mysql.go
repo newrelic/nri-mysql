70d11e6ebca2b2cb6db7f83125a255a7a12d88eb
Bryant Vinisky <bvinisky@gmail.com>
2021-06-30 04:57:07 -0700

    DB-3614: support passing optional parameters to the connection by usi… (#88)
    * DB-3614: support passing optional parameters to the connection by using flag -connection_parameters tls=skip-verify&timeout=5s
    
    * DB-3614: comment in go-sql-driver paramters link. Trim & trailing & from params and add a test for GenerateDSN when connection_parameters flag is provided.
    
    * DB-3614: renaming ConnectionParameters to ExtraConnectionURLArgs to be consistent with the naming in other New Relic integrations.
    
    * DB-3614: Improve help example with attr1=val1&attr2=val2 to include separator. Use url.Values to capture and encode connections parameters. Added -enable_tls and -insecure_skip_verify to argument list for friendlier config method to control these settings.

--- src/mysql.go
+++ src/mysql.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"runtime"
 	"strconv"
@@ -24,26 +25,44 @@ const (
 
 type argumentList struct {
 	sdk_args.DefaultArgumentList
-	Hostname              string `default:"localhost" help:"Hostname or IP where MySQL is running."`
-	Port                  int    `default:"3306" help:"Port on which MySQL server is listening."`
-	Username              string `help:"Username for accessing the database."`
-	Password              string `help:"Password for the given user."`
-	Database              string `help:"Database name"`
-	RemoteMonitoring      bool   `default:"false" help:"Identifies the monitored entity as 'remote'. In doubt: set to true"`
-	ExtendedMetrics       bool   `default:"false" help:"Enable extended metrics"`
-	ExtendedInnodbMetrics bool   `default:"false" help:"Enable InnoDB extended metrics"`
-	ExtendedMyIsamMetrics bool   `default:"false" help:"Enable MyISAM extended metrics"`
-	OldPasswords          bool   `default:"false" help:"Allow old passwords: https://dev.mysql.com/doc/refman/5.6/en/server-system-variables.html#sysvar_old_passwords"`
-	ShowVersion           bool   `default:"false" help:"Print build information and exit"`
+	Hostname               string `default:"localhost" help:"Hostname or IP where MySQL is running."`
+	Port                   int    `default:"3306" help:"Port on which MySQL server is listening."`
+	Username               string `help:"Username for accessing the database."`
+	Password               string `help:"Password for the given user."`
+	Database               string `help:"Database name"`
+	ExtraConnectionURLArgs string `help:"Specify extra connection parameters as attr1=val1&attr2=val2."` // https://github.com/go-sql-driver/mysql#parameters
+	InsecureSkipVerify     bool   `default:"false" help:"Skip verification of the server's certificate when using TLS with the connection."`
+	EnableTLS              bool   `default:"false" help:"Use a secure (TLS) connection."`
+	RemoteMonitoring       bool   `default:"false" help:"Identifies the monitored entity as 'remote'. In doubt: set to true"`
+	ExtendedMetrics        bool   `default:"false" help:"Enable extended metrics"`
+	ExtendedInnodbMetrics  bool   `default:"false" help:"Enable InnoDB extended metrics"`
+	ExtendedMyIsamMetrics  bool   `default:"false" help:"Enable MyISAM extended metrics"`
+	OldPasswords           bool   `default:"false" help:"Allow old passwords: https://dev.mysql.com/doc/refman/5.6/en/server-system-variables.html#sysvar_old_passwords"`
+	ShowVersion            bool   `default:"false" help:"Print build information and exit"`
 }
 
 func generateDSN(args argumentList) string {
-	params := ""
+	// Format query parameters
+	query := url.Values{}
 	if args.OldPasswords {
-		params = "?allowOldPasswords=true"
+		query.Add("allowOldPasswords", "true")
+	}
+	if args.EnableTLS {
+		query.Add("tls", "true")
+	}
+	if args.InsecureSkipVerify {
+		query.Add("tls", "skip-verify")
+	}
+	extraArgsMap, err := url.ParseQuery(args.ExtraConnectionURLArgs)
+	if err == nil {
+		for k, v := range extraArgsMap {
+			query.Add(k, v[0])
+		}
+	} else {
+		log.Warn("Could not successfully parse ExtraConnectionURLArgs.", err.Error())
 	}
 
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s%s", args.Username, args.Password, args.Hostname, args.Port, args.Database, params)
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?%s", args.Username, args.Password, args.Hostname, args.Port, args.Database, query.Encode())
 }
 
 var (
