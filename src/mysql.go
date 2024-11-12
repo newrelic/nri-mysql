30d7015dd6e3de7a6323072bdcdb5a12c752dbfa
tharun0064 <tbalanagu@newrelic.com>
2024-11-12 18:40:51 +0530

    init changes

--- src/mysql.go
+++ src/mysql.go
@@ -3,18 +3,18 @@ package main
 
 import (
 	"fmt"
+	sdk_args "github.com/newrelic/infra-integrations-sdk/v3/args"
+	"github.com/newrelic/infra-integrations-sdk/v3/data/attribute"
+	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
+	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
+	"github.com/newrelic/nri-mysql/src/query-performance-details"
 	"net"
 	"net/url"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
-
-	sdk_args "github.com/newrelic/infra-integrations-sdk/v3/args"
-	"github.com/newrelic/infra-integrations-sdk/v3/data/attribute"
-	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
-	"github.com/newrelic/infra-integrations-sdk/v3/integration"
-	"github.com/newrelic/infra-integrations-sdk/v3/log"
 )
 
 const (
@@ -24,21 +24,22 @@ const (
 
 type argumentList struct {
 	sdk_args.DefaultArgumentList
-	Hostname               string `default:"localhost" help:"Hostname or IP where MySQL is running."`
-	Port                   int    `default:"3306" help:"Port on which MySQL server is listening."`
-	Socket                 string `default:"" help:"MySQL Socket file."`
-	Username               string `help:"Username for accessing the database."`
-	Password               string `help:"Password for the given user."`
-	Database               string `help:"Database name"`
-	ExtraConnectionURLArgs string `help:"Specify extra connection parameters as attr1=val1&attr2=val2."` // https://github.com/go-sql-driver/mysql#parameters
-	InsecureSkipVerify     bool   `default:"false" help:"Skip verification of the server's certificate when using TLS with the connection."`
-	EnableTLS              bool   `default:"false" help:"Use a secure (TLS) connection."`
-	RemoteMonitoring       bool   `default:"false" help:"Identifies the monitored entity as 'remote'. In doubt: set to true"`
-	ExtendedMetrics        bool   `default:"false" help:"Enable extended metrics"`
-	ExtendedInnodbMetrics  bool   `default:"false" help:"Enable InnoDB extended metrics"`
-	ExtendedMyIsamMetrics  bool   `default:"false" help:"Enable MyISAM extended metrics"`
-	OldPasswords           bool   `default:"false" help:"Allow old passwords: https://dev.mysql.com/doc/refman/5.6/en/server-system-variables.html#sysvar_old_passwords"`
-	ShowVersion            bool   `default:"false" help:"Print build information and exit"`
+	Hostname                         string `default:"65.2.189.51" help:"Hostname or IP where MySQL is running."`
+	Port                             int    `default:"3306" help:"Port on which MySQL server is listening."`
+	Socket                           string `default:"" help:"MySQL Socket file."`
+	Username                         string `default:"root" help:"Username for accessing the database."`
+	Password                         string `default:"Admin@123" help:"Password for the given user."`
+	Database                         string `help:"Database name"`
+	ExtraConnectionURLArgs           string `help:"Specify extra connection parameters as attr1=val1&attr2=val2."` // https://github.com/go-sql-driver/mysql#parameters
+	InsecureSkipVerify               bool   `default:"false" help:"Skip verification of the server's certificate when using TLS with the connection."`
+	EnableTLS                        bool   `default:"false" help:"Use a secure (TLS) connection."`
+	RemoteMonitoring                 bool   `default:"false" help:"Identifies the monitored entity as 'remote'. In doubt: set to true"`
+	ExtendedMetrics                  bool   `default:"false" help:"Enable extended metrics"`
+	ExtendedInnodbMetrics            bool   `default:"false" help:"Enable InnoDB extended metrics"`
+	ExtendedMyIsamMetrics            bool   `default:"false" help:"Enable MyISAM extended metrics"`
+	OldPasswords                     bool   `default:"false" help:"Allow old passwords: https://dev.mysql.com/doc/refman/5.6/en/server-system-variables.html#sysvar_old_passwords"`
+	ShowVersion                      bool   `default:"false" help:"Print build information and exit"`
+	EnableQueryPerformanceMonitoring bool   `default:"true" help:"Enable query performance monitoring"`
 }
 
 func generateDSN(args argumentList) string {
@@ -109,30 +110,37 @@ func main() {
 	}
 
 	log.SetupLogging(args.Verbose)
+	//
+	//e, err := createNodeEntity(i, args.RemoteMonitoring, args.Hostname, args.Port)
+	//fatalIfErr(err)
+	//
+	//db, err := openDB(generateDSN(args))
+	//fatalIfErr(err)
+	//defer db.close()
+	//
+	//rawInventory, rawMetrics, err := getRawData(db)
+	//fatalIfErr(err)
+	//
+	//if args.HasInventory() {
+	//	populateInventory(e.Inventory, rawInventory)
+	//}
+	//
+	//if args.HasMetrics() {
+	//	ms := metricSet(
+	//		e,
+	//		"MysqlSample",
+	//		args.Hostname,
+	//		args.Port,
+	//		args.RemoteMonitoring,
+	//	)
+	//	populateMetrics(ms, rawMetrics)
+	//}
+	fmt.Println("heyyyyasasasas")
+	// New functionality
+	if args.EnableQueryPerformanceMonitoring {
+		fmt.Println("heyyyy")
+		query_performance_details.PopulateQueryPerformanceMetrics(args)
 
-	e, err := createNodeEntity(i, args.RemoteMonitoring, args.Hostname, args.Port)
-	fatalIfErr(err)
-
-	db, err := openDB(generateDSN(args))
-	fatalIfErr(err)
-	defer db.close()
-
-	rawInventory, rawMetrics, err := getRawData(db)
-	fatalIfErr(err)
-
-	if args.HasInventory() {
-		populateInventory(e.Inventory, rawInventory)
-	}
-
-	if args.HasMetrics() {
-		ms := metricSet(
-			e,
-			"MysqlSample",
-			args.Hostname,
-			args.Port,
-			args.RemoteMonitoring,
-		)
-		populateMetrics(ms, rawMetrics)
 	}
 
 	fatalIfErr(i.Publish())
