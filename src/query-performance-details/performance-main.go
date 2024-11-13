bd792d10e0e448114e5e53d63253276ea409eef7
tharun0064 <tbalanagu@newrelic.com>
2024-11-13 16:03:30 +0530

    log information

--- src/query-performance-details/performance-main.go
+++ src/query-performance-details/performance-main.go
@@ -42,30 +42,47 @@ func generateDSN(args arguments.ArgumentList) string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", args.Username, args.Password, mysqlURL, args.Database, query.Encode())
 }
 
+// main
 func PopulateQueryPerformanceMetrics(args arguments.ArgumentList) {
 	dsn := generateDSN(args)
 	db, err := openDB(dsn)
 	fatalIfErr(err)
 	defer db.close()
 
-	performanceSchemaEnabled, err := isPerformanceSchemaEnabled(db)
-	if !performanceSchemaEnabled {
-		fmt.Println("Performance Schema is not enabled. Skipping validation.")
+	isPreConditionsPassed := validatePreconditions(db)
+	if !isPreConditionsPassed {
+		fmt.Println("Preconditions failed. Exiting.")
 		return
+	}
+
+}
+
+func validatePreconditions(db dataSource) bool {
+
+	performanceSchemaEnabled, errPerformanceEnabled := isPerformanceSchemaEnabled(db)
+	if errPerformanceEnabled != nil {
+		log.Error("Failed to check Performance Schema status: %v", errPerformanceEnabled)
+		return false
+	}
+
+	if !performanceSchemaEnabled {
+		log.Error("Performance Schema is not enabled. Skipping validation.")
+		logEnablePerformanceSchemaInstructions(db)
+		return false
 	}
 
 	errEssentialConsumers := checkEssentialConsumers(db)
 	if errEssentialConsumers != nil {
-		fmt.Printf("Essential consumer check failed\n")
-		return
+		log.Error("Essential consumer check failed\n")
+		return false
 	}
 
 	errEssentialInstruments := checkEssentialInstruments(db)
 	if errEssentialInstruments != nil {
-		fmt.Printf("Essential instruments check failed\n")
-		return
+		log.Error("Essential instruments check failed\n")
+		return false
 	}
-
+	return true
 }
 
 func isPerformanceSchemaEnabled(db dataSource) (bool, error) {
@@ -73,7 +90,7 @@ func isPerformanceSchemaEnabled(db dataSource) (bool, error) {
 	rows, err := db.queryX("SHOW GLOBAL VARIABLES LIKE 'performance_schema';")
 
 	if !rows.Next() {
-		fmt.Println("No rows found")
+		log.Error("No rows found")
 		return false, nil
 	}
 
@@ -181,6 +198,49 @@ func checkEssentialInstruments(db dataSource) error {
 	}
 
 	return nil
+}
+
+func logEnablePerformanceSchemaInstructions(db dataSource) {
+	version, err := getMySQLVersion(db)
+	if err != nil {
+		log.Error("Failed to get MySQL version: %v", err)
+		return
+	}
+
+	log.Error("To enable the Performance Schema, add the following line to your MySQL configuration file (my.cnf or my.ini) and restart the MySQL server:")
+	log.Error("performance_schema=ON")
+
+	if strings.HasPrefix(version, "5.6") {
+		log.Error("For MySQL 5.6, you may also need to set the following variables:")
+		log.Error("performance_schema_instrument='%=ON'")
+		log.Error("performance_schema_consumer_events_statements_current=ON")
+		log.Error("performance_schema_consumer_events_statements_history=ON")
+		log.Error("performance_schema_consumer_events_statements_history_long=ON")
+		log.Error("performance_schema_consumer_events_waits_current=ON")
+		log.Error("performance_schema_consumer_events_waits_history=ON")
+		log.Error("performance_schema_consumer_events_waits_history_long=ON")
+	} else if strings.HasPrefix(version, "5.7") || strings.HasPrefix(version, "8.0") {
+		log.Error("For MySQL 5.7 and 8.0, you may also need to set the following variables:")
+		log.Error("performance_schema_instrument='%=ON'")
+		log.Error("performance_schema_consumer_events_statements_current=ON")
+		log.Error("performance_schema_consumer_events_statements_history=ON")
+		log.Error("performance_schema_consumer_events_statements_history_long=ON")
+		log.Error("performance_schema_consumer_events_waits_current=ON")
+		log.Error("performance_schema_consumer_events_waits_history=ON")
+		log.Error("performance_schema_consumer_events_waits_history_long=ON")
+	}
+}
+
+func getMySQLVersion(db dataSource) (string, error) {
+	var version string
+	row, err := db.queryX("SELECT VERSION();")
+	if row.Next() {
+		err = row.Scan(&version)
+	}
+	if err != nil {
+		return "", fmt.Errorf("failed to get MySQL version: %w", err)
+	}
+	return version, nil
 }
 
 func fatalIfErr(err error) {
