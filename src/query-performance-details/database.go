f1c1bdfe58373e5da93707daef1bb05587044e63
tharun0064 <tbalanagu@newrelic.com>
2024-11-13 16:11:45 +0530

    refactoring code

--- src/query-performance-details/database.go
+++ src/query-performance-details/database.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
+	arguments "github.com/newrelic/nri-mysql/src/args"
+	"net"
+	"net/url"
+	"strconv"
 )
 
 type dataSource interface {
@@ -34,4 +39,34 @@ func (db *database) queryX(query string) (*sqlx.Rows, error) {
 	rows, err := db.source.Queryx(query)
 	fatalIfErr(err)
 	return rows, err
+}
+
+func generateDSN(args arguments.ArgumentList) string {
+	query := url.Values{}
+	if args.OldPasswords {
+		query.Add("allowOldPasswords", "true")
+	}
+	if args.EnableTLS {
+		query.Add("tls", "true")
+	}
+	if args.InsecureSkipVerify {
+		query.Add("tls", "skip-verify")
+	}
+	extraArgsMap, err := url.ParseQuery(args.ExtraConnectionURLArgs)
+	if err == nil {
+		for k, v := range extraArgsMap {
+			query.Add(k, v[0])
+		}
+	} else {
+		log.Warn("Could not successfully parse ExtraConnectionURLArgs.", err.Error())
+	}
+	if args.Socket != "" {
+		log.Info("Socket parameter is defined, ignoring host and port parameters")
+		return fmt.Sprintf("%s:%s@unix(%s)/%s?%s", args.Username, args.Password, args.Socket, args.Database, query.Encode())
+	}
+
+	// Convert hostname and port to DSN address format
+	mysqlURL := net.JoinHostPort(args.Hostname, strconv.Itoa(args.Port))
+
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", args.Username, args.Password, mysqlURL, args.Database, query.Encode())
 }
